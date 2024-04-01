0ab1ba5c8dca6ada1e88003503480f29cc912aa4
GerryHorror <GerryHorror@users.noreply.github.com>
2024-04-02 00:06:02 +0200

    Enhanced contact page with simulated form submission feedback. Changes include adding JavaScript for form submission feedback, preventing default action, manipulating form visibility, and delayed display changes. On form submission, the form fades out, loading spinner appears, and success message displays. JavaScript block and CSS transitions are responsible for handling form submission events and form element properties.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,3 +10,27 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+//<-------------------------------------------------------------------------------------->
+// Contact Page
+/* This script is used to simulate a successful form submission by hiding the form
+and showing a success message after a delay*/
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contact-form');
+    const loadingSpinner = document.querySelector('.loading-spinner');
+    const successMessage = document.querySelector('.success-message');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        form.style.opacity = 0;
+        setTimeout(() => {
+            form.style.display = 'none';
+            loadingSpinner.style.display = 'flex';
+            setTimeout(() => {
+                loadingSpinner.style.display = 'none';
+                successMessage.style.display = 'flex';
+                setTimeout(() => {
+                    successMessage.classList.add('show');
+                }, 100);
+            }, 2000);
+        }, 500);
+    });
+});
