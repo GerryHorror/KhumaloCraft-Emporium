2101037c8c6577c5c0e97292b6e84d7600d3cdc7
GerryHorror <GerryHorror@users.noreply.github.com>
2024-04-17 17:36:35 +0200

    The changes made are as follows: Added `ProductID` property. Added `GetProducts` method

--- Models/productTable.cs
+++ Models/productTable.cs
@@ -9,6 +9,7 @@ namespace CLDVWebAppST10046280.Models
 
         public static SqlConnection con = new SqlConnection(con_string);
 
+        public int ProductID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
@@ -26,13 +27,44 @@ namespace CLDVWebAppST10046280.Models
                     cmd.Parameters.AddWithValue("@Price", m.Price);
                     cmd.Parameters.AddWithValue("@Category", m.Category);
                     cmd.Parameters.AddWithValue("@Available", m.Available);
-
                     con.Open();
                     int rowsAffected = cmd.ExecuteNonQuery();
                     con.Close();
 
                     return rowsAffected;
                 }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                throw ex;
+            }
+        }
+
+        // Add a method to retrieve all products from the database
+        public static List<productTable> GetProducts()
+        {
+            List<productTable> products = new List<productTable>();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(con_string))
+                {
+                    string sql = "SELECT * FROM productTable";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        productTable product = new productTable();
+                        product.ProductID = Convert.ToInt32(dr["productID"]);
+                        product.Name = dr["productName"].ToString();
+                        product.Price = Convert.ToDecimal(dr["productPrice"]);
+                        product.Category = dr["productCategory"].ToString();
+                        product.Available = Convert.ToBoolean(dr["productAvailability"]);
+                        products.Add(product);
+                    }
+                }
+                return products;
             }
             catch (Exception ex)
             {
